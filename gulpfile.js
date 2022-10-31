0037c682ad05349ef1e728b5fc335b720feb2b7a
mikehike123@gmail.com <mikehike123@gmail.com>
2022-10-31 10:48:31 -0700

    Added mailchimp contact form

--- gulpfile.js
+++ gulpfile.js
@@ -4,13 +4,13 @@ const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 
 function buildStyles() {
-  return gulp.src('./sass/*.scss')
+  return gulp.src('./src/sass/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css'));
+    .pipe(gulp.dest('./src/css'));
 };
 
 const watch = function(){
-  gulp.watch('./sass/**/*.scss',buildStyles);
+  gulp.watch('./src/sass/**/*.scss',buildStyles);
 };
 exports.default=gulp.parallel(buildStyles,gulp.series(watch));
 
